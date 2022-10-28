ff66bfc66a6872fb933818e12b8a325f88c89196
ABR-Developer <bsef19m005@pucit.edu.pk>
2022-10-28 12:37:58 +0500

    Completed 26

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,14 +3,14 @@ import React, { Component } from "react";
 export default class NewsItem extends Component {
 
   render() {
-    let { title, description , imageUrl, newsURL} = this.props;
+    let { title, description , imageUrl, newsUrl} = this.props;
     return (
       <div className="card my-2" style={{width : "18rem"}}>
-        <img src={imageUrl} className="card-img-top" alt="..." />
+        <img src={imageUrl} style={{height : '140px' , width: '100%'}} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="/" className="btn btn-sm btn-primary">
+          <h5 className="card-title" style={{height : '48px' , overflow: 'hidden'}}>{title}</h5>
+          <p className="card-text" style={{height : '100px' , overflow: 'hidden'}}>{description}</p>
+          <a href={newsUrl} className="btn btn-sm btn-primary">
             Read More
           </a>
         </div>
